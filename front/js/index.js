a8e514cba31bdd8cbb59c61a55f5d2801c960595
Rhomard <romhaar@gmail.com>
2022-04-04 11:43:42 +0200

    new version with only a fetch

--- front/js/index.js
+++ front/js/index.js
@@ -1,91 +1,31 @@
-// Fonction pour créer mes cartes
-// const kanapData = () => {
-//       fetch("http://localhost:3000/api/products")
-//             .then(function (res) {
-//                   if (res.ok) {
-//                         return res.json();
-//                   }
-//             })
-//             .then(function (data) {
-//                   // Boucle de création de Kanap (for each)
-//                   console.log(data.name);
-//                   document.getElementById("items").innerText = data.name;
-//             })
-//             .catch(function (err) {
-//                   console.log("Error with Kanap API.");
-//             });
-// };
-// kanapData();
+fetch("http://localhost:3000/api/products")
+      .then(function (res) {
+            if (res.ok) {
+                  return res.json();
+            }
+      })
+      .then(function (kanapData) {
+            for (let data of kanapData) {
+                  const link = document.createElement("a");
+                  items.appendChild(link);
 
-const getKanapData = () => {
-      fetch("http://localhost:3000/api/products")
-            .then(function (res) {
-                  if (res.ok) {
-                        return res.json();
-                  }
-            })
-            .then(function (kanapData) {
-                  console.log(kanapData);
+                  const article = document.createElement("article");
+                  link.appendChild(article);
 
-                  const createKanapCard = () => {
-                        const items = document.getElementById("items");
+                  const img = document.createElement("img");
+                  article.appendChild(img);
+                  img.src = data.imageUrl;
+                  img.alt = data.altTxt;
 
-                        const link = document.createElement("a");
-                        items.appendChild(link);
+                  const title = document.createElement("h3");
+                  article.appendChild(title);
+                  title.innerText = data.name;
 
-                        const article = document.createElement("article");
-                        link.appendChild(article);
-
-                        const img = document.createElement("img");
-                        article.appendChild(img);
-                        img.src = kanapData[i].imageUrl;
-                        img.alt = kanapData[i].altTxt;
-
-                        const title = document.createElement("h3");
-                        article.appendChild(title);
-                        title.innerText = kanapData[i].name;
-
-                        const description = document.createElement("p");
-                        article.appendChild(description);
-                        description.innerText = kanapData[i].description;
-                  };
-                  for (var i = 0; i < kanapData.length; i++) {
-                        createKanapCard();
-                  }
-            })
-            .catch(function (err) {
-                  console.log(err);
-            });
-};
-
-getKanapData();
-
-// const run = () => {
-//       for (let i = 0; i < kanapData.length; i++) {
-//             KanapData();
-//       }
-//       run();
-
-// // Je crée ma carte
-// const createKanapCard = () => {
-//       const items = document.getElementById("items");
-
-//       const link = document.createElement("a");
-//       items.appendChild(link);
-
-//       const article = document.createElement("article");
-//       link.appendChild(article);
-
-//       const img = document.createElement("img");
-//       article.appendChild(img);
-//       img.src = "http://localhost:3000/images/kanap01.jpeg";
-//       img.alt = "Photo d'un canapé bleu, deux places";
-
-//       const title = document.createElement("h3");
-//       article.appendChild(title);
-//       title.innerText = getDataKanap[i].name;
-
-//       const description = document.createElement("p");
-//       article.appendChild(description);
-//       description.innerHTML = "Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nullanisl arcu.";
-// };
+                  const description = document.createElement("p");
+                  article.appendChild(description);
+                  description.innerText = data.description;
+            }
+      })
+      .catch(function (err) {
+            console.log(err);
+      });
